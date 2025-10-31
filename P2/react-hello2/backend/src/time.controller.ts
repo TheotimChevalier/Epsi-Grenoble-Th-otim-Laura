1663e77efe63a2f3e1ec79cdc085aa40a575fe09
Laura-baratier <161334955+lorra7@users.noreply.github.com>
2025-10-31 09:55:00 +0100

    update

--- P2/react-hello2/backend/src/time.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-@Controller()
-export class TimeController {
-  @Get('time')
-  getTime() {
-    return { serverTime: new Date().toISOString() };
-  }
-}
